89b964409c0ae5902c7d2a514ab6786f3f27a59e
albertoh8 <albertoherrero888@gmail.com>
2022-10-14 17:25:16 +0200

    Creo la vista de la aplicacion ToGoodToGo

---
+++ app/src/main/java/com/albertoherjim/pmdm2022/Ut01/Ex04/Ut01Ex04ToGoodToGo_Activity.kt
@@ -0,0 +1,12 @@
+package com.albertoherjim.pmdm2022.Ut01.Ex04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.albertoherjim.pmdm2022.R
+
+class Ut01Ex04ToGoodToGo_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01_ex04_to_good_to_go)
+    }
+}
